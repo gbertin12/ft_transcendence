ea167cfdc30b2d05dd62ab3dfe482ef97a04e6b5
Jonas ALLERHAND <jallerha@student.42.fr>
2023-04-13 19:39:41 +0200

    add: basic test model for `Channel` on prisma

--- app/back/src/app.module.ts
+++ app/back/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/db.module';
+import { ChannelModule } from './channel/channel.module';
 
 @Module({
-    imports: [AuthModule, DbModule],
+    imports: [AuthModule, DbModule, ChannelModule],
 })
 export class AppModule {}
