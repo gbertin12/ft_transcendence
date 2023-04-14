7c8f07b5c4fcc7be64ecce8f9892dd20cbf6d886
Jonas ALLERHAND <jallerha@student.42.fr>
2023-04-14 13:25:31 +0200

    fix: make the browser scrollable

--- app/front/src/components/chat/ChannelsBrowser.tsx
+++ app/front/src/components/chat/ChannelsBrowser.tsx
@@ -32,38 +32,40 @@ const ChannelsBrowser: React.FC<ChannelsBrowserProps> = ({ privateMessages, defa
 	if (!channels) return <li>Failed to fetch data</li>;
 
 	return (
-		<div className="pl-5 pt-2 py-1 bg-blue-950">
-			<h2 className="text-3xl text-white">Salons</h2>
-			<ul className="pl-2 pt-1">
-				{channels.map((channel: any, index: number) => (
-					<li
-						className={`text-gray-200 flex gap-x-3 rounded-lg px-2 py-1 my-1 mr-2 cursor-pointer ${
-							selectedIndex === index ? "bg-blue-800" : "hover:bg-blue-500"
-						}`}
-						key={channel}
-						onClick={() => changeFocus(index)}
-					>
-						<h3 className="text-lg">#</h3>
-						<h3 className="text-lg">{channel.title}</h3>
-					</li>
-				))}
-			</ul>
-			<hr className="border-slate-100 my-4 w-2/3 mx-auto" />
-			<h2 className="text-3xl text-white">Messages privés</h2>
-			<ul className="pl-2 pt-1">
-				{privateMessages.map((privateMessage, index) => (
-					<li
-						className={`text-gray-200 flex gap-x-3 rounded-lg px-2 py-1 my-1 mr-2 cursor-pointer ${
-							selectedIndex === index + channels.length ? "bg-blue-800" : "hover:bg-blue-500"
-						}`}
-						key={privateMessage}
-						onClick={() => changeFocus(index + channels.length)}
-					>
-						<h3 className="text-lg">@</h3>
-						<h3 className="text-lg">{privateMessage}</h3>
-					</li>
-				))}
-			</ul>
+		<div className="pl-5 pt-2 py-1 bg-blue-950 overflow-hidden">
+			<div className="h-full overflow-auto">
+				<h2 className="text-3xl text-white">Salons</h2>
+				<ul className="pl-2 pt-1">
+					{channels.map((channel: any, index: number) => (
+						<li
+							className={`text-gray-200 flex gap-x-3 rounded-lg px-2 py-1 my-1 mr-2 cursor-pointer ${
+								selectedIndex === index ? "bg-blue-800" : "hover:bg-blue-500"
+							}`}
+							key={channel}
+							onClick={() => changeFocus(index)}
+						>
+							<h3 className="text-lg">#</h3>
+							<h3 className="text-lg">{channel.title}</h3>
+						</li>
+					))}
+				</ul>
+				<hr className="border-slate-100 my-4 w-2/3 mx-auto" />
+				<h2 className="text-3xl text-white">Messages privés</h2>
+				<ul className="pl-2 pt-1">
+					{privateMessages.map((privateMessage, index) => (
+						<li
+							className={`text-gray-200 flex gap-x-3 rounded-lg px-2 py-1 my-1 mr-2 cursor-pointer ${
+								selectedIndex === index + channels.length ? "bg-blue-800" : "hover:bg-blue-500"
+							}`}
+							key={privateMessage}
+							onClick={() => changeFocus(index + channels.length)}
+						>
+							<h3 className="text-lg">@</h3>
+							<h3 className="text-lg">{privateMessage}</h3>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
