c7a39a4ae9338fa8f9a67a00251b40100c0fbb34
Jonas ALLERHAND <jallerha@student.42.fr>
2023-04-14 14:59:38 +0200

    add: basic message component

---
+++ app/front/src/components/chat/Message.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface MessageProps {
+    timestamp: number;
+    userId?: number;
+    content: string;
+}
+
+const Message: React.FC<MessageProps> = ({ timestamp, userId, content }) => {
+    return (
+        <li className="grid my-3">
+            {/* <img
+                className="w-12 h-12 rounded-full"
+                src=""
+            /> */}
+            {/* TODO: fix rendering */}
+            <div className="w-12 h-12 rounded-full bg-gray-300"></div>
+            <div>
+                <p className="text-gray-500 text-sm">
+                    {new Date(timestamp).toLocaleString()}
+                </p>
+                <p>{content}</p>
+            </div>
+        </li>
+    )
+};
+
+export default Message;
