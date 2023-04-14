b74aefb3d20793c5894d6ad4a567effd7dce016f
Jonas ALLERHAND <jallerha@student.42.fr>
2023-04-14 13:11:51 +0200

    fix: issue with indexes on channel browser

--- app/front/src/components/chat/ChannelsBrowser.tsx
+++ app/front/src/components/chat/ChannelsBrowser.tsx
@@ -27,6 +27,7 @@ const ChannelsBrowser: React.FC<ChannelsBrowserProps> = ({ privateMessages, defa
 		setSelectedIndex(index);
 	};
 
+	// TODO: Prettify this
 	if (isLoading) return <li>Loading...</li>;
 	if (!channels) return <li>Failed to fetch data</li>;
 
@@ -34,13 +35,13 @@ const ChannelsBrowser: React.FC<ChannelsBrowserProps> = ({ privateMessages, defa
 		<div className="pl-5 pt-2 py-1 bg-blue-950">
 			<h2 className="text-3xl text-white">Salons</h2>
 			<ul className="pl-2 pt-1">
-				{channels.map((channel: any) => (
+				{channels.map((channel: any, index: number) => (
 					<li
 						className={`text-gray-200 flex gap-x-3 rounded-lg px-2 py-1 my-1 mr-2 cursor-pointer ${
-							selectedIndex === channel.id ? "bg-blue-800" : "hover:bg-blue-500"
+							selectedIndex === index ? "bg-blue-800" : "hover:bg-blue-500"
 						}`}
 						key={channel}
-						onClick={() => changeFocus(channel.id)}
+						onClick={() => changeFocus(index)}
 					>
 						<h3 className="text-lg">#</h3>
 						<h3 className="text-lg">{channel.title}</h3>
