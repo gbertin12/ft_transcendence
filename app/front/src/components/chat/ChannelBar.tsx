28356452fc3e2efae6756472aef2f2486fd2cd9a
Jonas ALLERHAND <jallerha@student.42.fr>
2023-04-14 13:50:20 +0200

    add: test channel bar

---
+++ app/front/src/components/chat/ChannelBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface ChannelBarProps {
+    title: string;
+    topic: string;
+    isPrivateMessage?: boolean;
+    // messages: any[]; // -- TODO
+}
+
+const ChannelBar: React.FC<ChannelBarProps> = ({title, topic, isPrivateMessage}) => {
+    return (
+        <div className="pl-4 bg-blue-900 flex gap-x-1 w-full h-12 text-white">
+            <h1 className="my-auto text-2xl">{isPrivateMessage ? "@" : "#"}</h1>
+            <h2 className="my-auto text-2xl">{title}</h2>
+            {/* <h3 className="my-auto text-gray-100 text-lg">{topic}</h3> */}
+        </div>
+    );
+};
+
+export default ChannelBar;
