d5025736a09313bd87984dd8805ea2a731a52435
Jonas ALLERHAND <jallerha@student.42.fr>
2023-04-16 11:41:12 +0200

    update: `seed.ts` create channels

--- app/back/prisma/seed.ts
+++ app/back/prisma/seed.ts
@@ -1,42 +1,78 @@
 // prisma/seed.ts
 
 import { PrismaClient } from '@prisma/client'
-import { DbService } from 'src/db/db.service';
 import { faker } from '@faker-js/faker';
+import { sha512 } from 'js-sha512';
 
 const prisma = new PrismaClient()
 
-function createFakeChannel(size: number) {
+async function createFakeChannel(size: number, isPrivate: boolean = false, isPasswordProtected: boolean = false) {
     switch (size) {
         case 0: // short
-            return {
-                title: faker.word.noun(),
-                topic: faker.lorem.words(10),
-            };
+            return prisma.channel.create({
+                data: {
+                    name: faker.word.noun(),
+                    topic: faker.lorem.words(4),
+                    private: isPrivate,
+                    creation_date: faker.date.past().toISOString(),
+                    owner_id: 0,
+                    password: isPasswordProtected ? sha512(faker.internet.password()) : "",
+                }
+            });
         case 1: // long
-            return {
-                title: faker.word.noun() + "-" + faker.word.noun(),
-                topic: faker.lorem.words(21),
-            };
+            return prisma.channel.create({
+                data: {
+                    name: faker.word.noun() + "-" + faker.word.noun(),
+                    topic: faker.lorem.words(8),
+                    private: isPrivate,
+                    creation_date: faker.date.past().toISOString(),
+                    owner_id: 1,
+                    password: isPasswordProtected ? sha512(faker.internet.password()) : "",
+                }
+            });
         default:
             throw new Error("Invalid size");
     }
 }
 
-async function main() {
-    console.log("Creating fake channels with short names (30)");
-    for (let i = 0; i < 30; i++) {
-        await prisma.channel.create({
-            data: createFakeChannel(0),
-        })
-    }
-    console.log("Creating fake channels with long names (30)");
-    for (let i = 0; i < 30; i++) {
-        await prisma.channel.create({
-            data: createFakeChannel(1),
-        })
-    }
+async function seedUsers() {
+    await prisma.user.createMany({
+        data: [
+            {
+                id: 0,
+                username: "test",
+                password: sha512("test"),
+                role: "?"
+            },
+            {
+                id: 1,
+                username: "user",
+                password: sha512("user"),
+                role: "user"
+            },
+        ]
+    });
+}
 
+async function seedChannels() {
+    await prisma.channel.createMany({
+        data: [
+            await createFakeChannel(0, false, false),
+            await createFakeChannel(1, false, false),
+            await createFakeChannel(0, true, false),
+            await createFakeChannel(1, true, false),
+            await createFakeChannel(0, false, true),
+            await createFakeChannel(1, false, true)
+        ]
+    });
+}
+
+async function main() {
+    // reset all tables
+    await prisma.$executeRaw`TRUNCATE TABLE "User", "Channel", "Message" RESTART IDENTITY CASCADE`;
+
+    await seedUsers();
+    await seedChannels();
 }
 
 main()
