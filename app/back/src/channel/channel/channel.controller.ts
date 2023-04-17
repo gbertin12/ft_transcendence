ede06ca7acd423e586ad1d47c9ce06d975d11246
Jonas ALLERHAND <jallerha@student.42.fr>
2023-04-17 15:19:12 +0200

    update: send messages works

--- app/back/src/channel/channel/channel.controller.ts
+++ app/back/src/channel/channel/channel.controller.ts
@@ -21,4 +21,20 @@ export class ChannelController {
         }
         return await this.channelService.getMessages(channelId);
     }
+
+    @Post(':id/message')
+    async createMessage(@Param("id") id: string, @Body() body: any) {
+        let channelId: number = -1;
+        try {
+            channelId = parseInt(id);
+        } catch (error)                     { throw new HttpException('Invalid Channel ID', 400); }
+        if (!body || !body.content)         { throw new HttpException('Invalid Message', 400); }
+        if (body.content.length > 2000)     { throw new HttpException('Message too long', 400); }
+
+        // TODO: Check that the channel exists
+        // TODO: Check that the user is in the channel
+        // TODO: Get userId with session / cookies / token / whatever
+        let senderId = 1;
+        return await this.channelService.createMessage(senderId, channelId, body.content);
+    }
 }
--- app/back/src/channel/channel/channel.service.ts
+++ app/back/src/channel/channel/channel.service.ts
@@ -14,10 +14,20 @@ export class ChannelService {
             where: {
                 channel_id: id
             },
-            orderBy: { // sort from newest to oldest
-                timestamp: 'asc'
+            orderBy: { // sort from oldest to newest (front-end will reverse it)
+                timestamp: 'desc'
             },
             take: 50 // limit to 50 messages
+        });
+    }
+
+    async createMessage(senderId: number, id: number, content: string) {
+        return await this.db.message.create({
+            data: {
+                sender_id: senderId,
+                channel_id: id,
+                content: content
+            }
         });
     }
 }
