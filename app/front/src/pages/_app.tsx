fb776d21639665749352d024c880ac2477ef6def
Jonas ALLERHAND <jallerha@student.42.fr>
2023-04-26 10:54:29 +0200

    add: implement default dark / light

--- app/front/src/pages/_app.tsx
+++ app/front/src/pages/_app.tsx
@@ -1,6 +1,32 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme } from "@nextui-org/react"
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+const lightTheme = createTheme({
+	type: 'light',
+})
+
+const darkTheme = createTheme({
+	type: 'dark',
+})
+
+
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<NextThemesProvider
+			defaultTheme='system'
+			attribute='class'
+			value={{
+				light: lightTheme.className,
+				dark: darkTheme.className
+			}}
+		>
+			<NextUIProvider>
+				<Component {...pageProps} />
+			</NextUIProvider>
+		</NextThemesProvider>
+	);
 }
