f87aee383977fe0200d7174bfef4153f1fbbd07c
Jonas ALLERHAND <jallerha@student.42.fr>
2023-05-01 12:15:35 +0200

    add: validator pipeline for DTOs

--- app/back/src/main.ts
+++ app/back/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -13,6 +14,14 @@ async function bootstrap() {
     };
 
     app.enableCors(options); // Bypass CORS
+
+    app.useGlobalPipes(new ValidationPipe(
+		{
+			exceptionFactory: (validationErrors: ValidationError[] = []) => {
+				return new BadRequestException(validationErrors);
+			},
+		}
+	)); // Use class-validator
 
     await app.listen(3000);
 }
