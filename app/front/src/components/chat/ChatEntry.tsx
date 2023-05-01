ba3785ba90aad6c3f19c9678320492d4f60c7f22
Jonas ALLERHAND <jallerha@student.42.fr>
2023-05-01 12:16:06 +0200

    update: `ChatEntry` with all possible states

--- app/front/src/components/chat/ChatEntry.tsx
+++ app/front/src/components/chat/ChatEntry.tsx
@@ -1,23 +1,66 @@
-import { Avatar, Badge, Container, Grid, Text } from "@nextui-org/react";
-import React from "react";
+import { Avatar, Badge, Grid, Text } from '@nextui-org/react';
+import React from 'react';
+import { FaComment } from 'react-icons/fa';
+import { FaEye, FaGamepad } from 'react-icons/fa';
 
-const ChatEntry: React.FC = () => {
+interface ChatEntryProps {
+    name: string;
+    userId: number;
+    isOnline: boolean;
+    isTyping: boolean;
+    isPlaying: boolean;
+    unreadMessages?: number;
+}
+
+function getStatusColor(isOnline: boolean, isPlaying: boolean) {
+    if (!isOnline) {
+        return "default";
+    }
+    if (isPlaying) {
+        return "error";
+    }
+    return "success";
+}
+
+const ChatEntry: React.FC<ChatEntryProps> = ({ name, userId, isOnline, isTyping, isPlaying, unreadMessages }) => {
+    if (unreadMessages === undefined) { unreadMessages = 2; }
     return (
-        <Grid.Container>
-            <Grid css={{w: "stretch"}}>
+        <Grid.Container gap={1}>
+            <Grid css={{w: "stretch"}} xs={2}>
                 <Badge
                     content=""
                     placement="bottom-right"
-                    color="success" // gray by default
-                    variant="dot"   // set to 'points' when typing
+                    color={getStatusColor(isOnline, isPlaying)}
+                    variant={isTyping ? "points" : "dot"}
                 >
                     <Avatar
                         size="lg"
                     />
                 </Badge>
-                <Text span css={{ml: 12}}>name</Text>
-                {/* TODO: Add a red badge when a message is unread */}
             </Grid>
+            <Grid xs={6} css={{my: "auto"}}>
+                <Text span>{name}</Text>
+            </Grid>
+            {(isPlaying ? (
+                <Grid xs={1} css={{my: "auto"}}>
+                    <FaEye size="md"/>
+                </Grid>
+            ) : (
+                <Grid xs={1} css={{my: "auto"}}>
+                    <FaGamepad size="md"/>
+                </Grid>
+            ))}
+            <Grid xs={1} css={{my: "auto"}}>
+                <Badge
+                    content={unreadMessages > 9 ? "9+" : unreadMessages.toString()}
+                    placement="bottom-right"
+                    color="error"
+                    isInvisible={(unreadMessages === 0)}
+                >
+                    <FaComment />
+                </Badge>
+            </Grid>
+
         </Grid.Container >
     );
 };
