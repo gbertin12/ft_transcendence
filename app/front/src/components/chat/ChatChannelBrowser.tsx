83bfb81cc2115035ba66012b93e3b85bf6ea4fc8
Jonas ALLERHAND <jallerha@student.42.fr>
2023-05-01 15:26:40 +0200

    add: proper channel browser

--- app/front/src/components/chat/ChatChannelBrowser.tsx
+++ app/front/src/components/chat/ChatChannelBrowser.tsx
@@ -1,18 +1,42 @@
-import { Avatar, Container } from "@nextui-org/react";
+import { Avatar, Container, Loading, Text } from "@nextui-org/react";
 import React from "react";
 import ChatEntry from "./ChatEntry";
 import ChannelEntry from "./ChannelEntry";
+import { Channel } from "@/interfaces/chat.interfaces";
 
-const ChatChannelBrowser: React.FC = () => {
+interface ChatChannelBrowserProps {
+    channelChanged: (channel: Channel) => void;
+    channels?: Channel[];
+}
+
+const ChatChannelBrowser: React.FC<ChatChannelBrowserProps> = ({ channelChanged, channels }) => {
+    const [selectedIndex, setSelectedIndex] = React.useState<number>(0);
+
+    if (!channels) {
+        return (
+            <Container css={{p: 0}}>
+                <Loading />
+            </Container>
+        );
+    }
+
     return (
         <Container css={{p: 0}}>
-            <ChannelEntry
-                name="general"
-                channelId={0}
-                hasPassword={false}
-                ownerId={0}
-                unreadMessages={1}
-            />
+            {channels.map((channel, index) => (
+                <ChannelEntry
+                    key={channel.id}
+                    name={channel.name}
+                    channelId={channel.id}
+                    hasPassword={false} // todo: return this value with the backend
+                    ownerId={channel.owner_id}
+                    unreadMessages={0}  // todo: return this value with the backend
+                    onClick={() => {
+                        setSelectedIndex(index);
+                        channelChanged(channel);
+                    }}
+                    isSelected={selectedIndex === index}
+                />
+            ))}
         </Container>
     );
 };
