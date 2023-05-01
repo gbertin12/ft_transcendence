5bedcf4623eb1f9ca5390144fd048ef212b71de5
Jonas ALLERHAND <jallerha@student.42.fr>
2023-05-01 15:26:28 +0200

    update: changing channels now works

--- app/front/src/components/chat/ChannelEntry.tsx
+++ app/front/src/components/chat/ChannelEntry.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Badge, Grid, Text } from '@nextui-org/react';
+import { Badge, Grid, Text } from '@nextui-org/react';
 import React from 'react';
 
 interface ChannelEntryProps {
@@ -6,26 +6,64 @@ interface ChannelEntryProps {
     channelId: number;
     hasPassword: boolean; // TODO: use this to display a lock icon
     ownerId: number;
+    isSelected: boolean;
+    onClick: () => void;
     unreadMessages?: number;
 }
 
-const ChannelEntry: React.FC<ChannelEntryProps> = ({ name, channelId, hasPassword, ownerId, unreadMessages }) => {
+function getBackgroundColor(isHovered: boolean, isSelected: boolean) {
+    if (isSelected) {
+        return "$accents1";
+    }
+    if (isHovered) {
+        return "$accents2";
+    }
+    return "transparent";
+}
+
+const ChannelEntry: React.FC<ChannelEntryProps> = ({ name, channelId, hasPassword, ownerId, isSelected, onClick, unreadMessages }) => {
     if (unreadMessages === undefined) { unreadMessages = 0; } // default to 0 (ugly hack)
+
+    const [isHovered, setIsHovered] = React.useState(false);
+
     return (
-        <Grid.Container gap={1}>
-            <Grid css={{w: "stretch"}} xs={2}>
+        <Grid.Container
+            gap={0.5}
+            as="a"
+            onClick={onClick}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            css={{
+                backgroundColor: getBackgroundColor(isHovered, isSelected),
+                borderRadius: "5px",
+                padding: "5px",
+                transition: "background-color 0.05s ease-in-out",
+            }}
+        >
+            <Grid xs={1} css={{my: "auto"}}>
                 <Badge
                     content={unreadMessages > 9 ? "9+" : unreadMessages.toString()}
                     placement="bottom-right"
                     color="error"
                     isInvisible={(unreadMessages === 0)}
-                    verticalOffset={22} // XXX: required for some reason (maybe fix it?)
                 >
-                    <Text h1 weight="bold">#</Text>
+                    <Text
+                        span
+                        size="$3xl"
+                        weight="bold"
+                    >
+                        #
+                    </Text>
                 </Badge>
             </Grid>
-            <Grid xs={6} css={{my: "auto"}}>
-                <Text h3>{name}</Text>
+            <Grid xs={11} css={{my: "auto"}}>
+                <Text
+                    span
+                    size="$xl"
+                    weight="bold"
+                >
+                    {name}
+                </Text>
             </Grid>
         </Grid.Container >
     );
