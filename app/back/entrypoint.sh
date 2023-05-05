b5beadfe72c69670875d6be25378297319a412d5
Jonas ALLERHAND <jallerha@student.42.fr>
2023-05-05 18:02:52 +0200

    update: entrypoint

--- app/back/entrypoint.sh
+++ app/back/entrypoint.sh
@@ -2,6 +2,5 @@
 
 # TODO: check if DB already exists before running the migration
 # DB should be in a volume for persistance
-npx prisma migrate dev --name init
-npx prisma db push
+npx prisma db push --accept-data-loss
 npm run start:dev
