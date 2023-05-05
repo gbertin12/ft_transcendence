dee82476c09a092e9ed986fa788cb84ae5af4ed9
Lara REALI <ereali@e2r2p3.clusters.42paris.fr>
2023-05-05 16:59:11 +0200

    modifie index

--- app/front/src/pages/index.tsx
+++ app/front/src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-    Home page
+    <h1>home</h1>
     </main>
 	</>
   )
