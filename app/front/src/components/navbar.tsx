035be3bad2ec26bc1cfde2c6795d0a28b2d12266
Neyune <56516084+Neyune@users.noreply.github.com>
2023-05-09 18:45:12 +0200

    modifie navbar to next ui

--- app/front/src/components/navbar.tsx
+++ app/front/src/components/navbar.tsx
@@ -1,37 +1,68 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 import Image from 'next/image';
 import { IconMedal } from '@tabler/icons-react';
 import { IconMessageCircle2 } from '@tabler/icons-react';
 import { IconDeviceGamepad } from '@tabler/icons-react';
+import { Navbar , Button, Link} from "@nextui-org/react";
 
 // <div className="grid text-center grid-cols-3 bg-[#DFDFDF] justify-items-center">
 // dark:bg-slate-800                                  dark:bg-[#121212]
-export default function Navbar() {
+
+export default function myNavbar() {
   return (
-	<>
-	<div className="flex flex-row justify-between items-center h-16 xl:h-20 bg-[#DFDFDF]">
-		<Link href="/" className="px-5 py-4">
-			Home
-		</Link>
+	      <Navbar isBordered variant="static">
+		  <Navbar.Brand>
+              ft_pong
+          </Navbar.Brand>
+	          <Navbar.Content gap={50}>
+	            <Navbar.Link href="/leaderboard">
+					<IconMedal size={50} strokeWidth={1.5}/>
+				</Navbar.Link>
+	            <Navbar.Link href="/game">
+					<IconDeviceGamepad size={50} strokeWidth={1.5}/>
+				</Navbar.Link>
+	            <Navbar.Link href="/chat">
+				<IconMessageCircle2 size={50} strokeWidth={1.5}/>
+				</Navbar.Link>
 
-	  <div className="flex flex-row justify-between justify-items-center w-1/3 ">
-		<Link href="/leaderboard">
-			<IconMedal size={50} strokeWidth={1.5}/>
-		</Link>
+	          </Navbar.Content>
+			  <Navbar.Content>
 
-		<Link href="/game ">
-			<IconDeviceGamepad size={50} strokeWidth={1.5}/>
-		</Link>
-	    <Link href="/chat">
-			<IconMessageCircle2 size={50} strokeWidth={1.5}/>
-	    </Link>
-	  </div>
+			  <Navbar.Link href="/auth">
+				  Se connecter
+			  </Navbar.Link>
+			  </Navbar.Content>
 
-		  <Link href="/auth"
-			  className="px-5 py-4">
-			Se connecter
-		</Link>
-		</div>
- 	</>
-	)
+	    </Navbar>
+  )
 }
+
+// export default function Navbar() {
+//   return (
+// 	<>
+// 	<div className="flex flex-row justify-between items-center h-16 xl:h-20 bg-[#DFDFDF]">
+// 		<Link href="/" className="px-5 py-4">
+// 			Home
+// 		</Link>
+//
+// 	  <div className="flex flex-row justify-between justify-items-center w-1/3 ">
+// 		<Link href="/leaderboard">
+// 			<IconMedal size={50} strokeWidth={1.5}/>
+// 		</Link>
+//
+// 		<Link href="/game ">
+// 			<IconDeviceGamepad size={50} strokeWidth={1.5}/>
+// 		</Link>
+// 	    <Link href="/chat">
+// 			<IconMessageCircle2 size={50} strokeWidth={1.5}/>
+// 	    </Link>
+// 	  </div>
+//
+// 		  <Link href="/auth"
+// 			  className="px-5 py-4">
+// 			Se connecter
+// 		</Link>
+// 		</div>
+//  	</>
+// 	)
+// }
