9be0c320d85f10af68df32e8cd2a7358c19b3808
Neyune <56516084+Neyune@users.noreply.github.com>
2023-05-18 16:21:00 +0200

    create dummy add to front

--- app/front/src/components/auth/SignUp.tsx
+++ app/front/src/components/auth/SignUp.tsx
@@ -1,5 +1,10 @@
 import { Input, Spacer, Button , Grid, Text} from "@nextui-org/react";
 
+
+function dummyLogin() {
+    window.location.href = 'http://localhost:3000/auth/dummy';
+}
+
 export default function SignIn() {
   return (
 		  <Grid>
@@ -15,11 +20,16 @@ export default function SignIn() {
 		 </Grid>
 		 <Spacer y={1}/>
 		 <Grid.Container justify='flex-end'>
-  	   <Grid >
-  	   <Button bordered color="$white" auto>
-  		 <Text >Sign up</Text>
-  	   </Button>
-  	   </Grid>
+		 <Grid >
+  	   <Button bordered color="$white" onClick={dummyLogin} auto>
+    		 <Text >Sign up</Text>
+    	   </Button>
+    	   </Grid>
+		<Grid >
+			<Button bordered color="$white" onClick={dummyLogin} auto>
+				 <Text >Dummy sign up</Text>
+			</Button>
+		</Grid>
   	   </Grid.Container>
   	   </Grid.Container>
   	   </Grid>
