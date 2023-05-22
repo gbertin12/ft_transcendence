2827f23a573f3ba44b584fdb695c5185ca2e9857
Jonas ALLERHAND <jallerha@student.42.fr>
2023-05-22 16:11:11 +0200

    fix: cache socket in socket context

--- app/front/src/contexts/socket.context.tsx
+++ app/front/src/contexts/socket.context.tsx
@@ -1,22 +1,19 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useMemo } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 interface SocketContextType {
   socket: Socket;
 }
 
+let socket: Socket = io('http://localhost:8001');
+
 const SocketContext = createContext<SocketContextType>({
-  socket: io('http://localhost:8001'),
+  socket,
 });
 
 export const useSocket = () => useContext(SocketContext);
 
 export const SocketContextProvider: React.FC<any> = ({ children }) => {
-  const socket = io('http://localhost:8001');
-
-  return (
-    <SocketContext.Provider value={{ socket }}>
-      {children}
-    </SocketContext.Provider>
-  );
-};
+  const socketValue = useMemo(() => ({ socket }), [socket]);
+  return <SocketContext.Provider value={socketValue}>{children}</SocketContext.Provider>;
+}
