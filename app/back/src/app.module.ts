40d37330dce69b7498fcb63b87da49cb10683180
Gael BERTIN <gbertin@e2r1p5.clusters.42paris.fr>
2023-05-31 11:45:06 +0200

    merge contexts

--- app/back/src/app.module.ts
+++ app/back/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/db.module';
 import { ChannelModule } from './channel/channel.module';
-import { ChatGateway } from "./gateway/chat.gateway";
 import { UserModule } from './user/user.module';
 import { PongModule } from './pong/pong.module';
 
 @Module({
-    imports: [AuthModule, DbModule, ChannelModule, ChatGateway, UserModule, PongModule],
+    imports: [AuthModule, DbModule, ChannelModule, UserModule, PongModule],
 })
 export class AppModule {}
