603c9f46555745de7410b0a3fda81a7a4a6b0fd9
yep <nope>
2023-05-31 15:43:17 +0200

    remove duplicate User interface + add `otp` field

--- app/back/src/interfaces/user.interface.ts
+++ app/back/src/interfaces/user.interface.ts
@@ -1,25 +1,17 @@
 export interface User {
-	id: number;
-	name: string;
-	avatar: string;
-	wins: number;
-	losses: number;
-	elo: number;
+    id: number;
+    name: string;
+    avatar: string;
+    wins: number;
+    losses: number;
+    elo: number;
+    otp: boolean;
 }
 
 export interface RowLeaderboard {
-	rank: number;
-	avatar: string;
-	name: string;
-	winrate: number;
-	elo: number;
-}
-
-export interface User {
-    id:        number;
-    name:      string;
-    avatar:    string;
-    wins:      number;
-    losses:    number;
-    elo:       number;
+    rank: number;
+    avatar: string;
+    name: string;
+    winrate: number;
+    elo: number;
 }
