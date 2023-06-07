35decadacbf126347e51dda41b217d02dbafffaa
Jonas ALLERHAND <jallerha@student.42.fr>
2023-06-07 19:00:05 +0200

    fix: dependency mistake

--- app/back/src/punishments/punishments.module.ts
+++ app/back/src/punishments/punishments.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PunishmentsService } from './punishments.service';
 import { PunishmentsController } from './punishments.controller';
-import { DbService } from 'db/db.service';
+import { DbService } from '../db/db.service';
 
 @Module({
   controllers: [PunishmentsController],
-  providers: [PunishmentsService],
-  imports: [DbService],
+  providers: [PunishmentsService, DbService],
+  imports: [],
   exports: [PunishmentsService]
 })
 export class PunishmentsModule {}
