26fe2ba4dbd7bcfcdeb33830deda2def83d4b062
Jonas ALLERHAND <jallerha@student.42.fr>
2023-06-11 21:20:04 +0200

    wip: dms

---
+++ app/front/src/components/chat/DMChatBox.tsx
@@ -0,0 +1,226 @@
+import React, { useEffect, useMemo, useState, useCallback } from "react";
+import { Channel, ChannelStaff, Message, MessageData, PunishmentData, User } from "@/interfaces/chat.interfaces";
+import { Button, Container, Grid, Text, Textarea } from "@nextui-org/react";
+import ChatMessage from "@/components/chat/ChatMessage";
+import { useUser } from '@/contexts/user.context';
+import ChannelPasswordPrompt from "./ChannelPasswordPrompt";
+import axios from "axios";
+import { useChat } from "@/contexts/chat.context";
+import { IconShieldCog } from "@tabler/icons-react";
+
+interface DMChatBoxProps {
+    interlocutor: User;
+}
+
+const DMChatBox: React.FC<DMChatBoxProps> = ({ interlocutor }) => {
+    const [missingPermissions, setMissingPermissions] = useState<boolean>(false);
+    const [messages, setMessages] = useState<MessageData[]>([]);
+    const [powerModalOpen, setPowerModalOpen] = useState<boolean>(false);
+    const [ownerId, setOwnerId] = useState<number>(-1);
+    const [admins, setAdmins] = useState<Set<number>>(new Set<number>());
+    const { socket, user } = useUser();
+    const {
+        bannedChannels,
+        setBannedChannels,
+        mutedChannels,
+        setMutedChannels,
+        blockedUsers,
+        setBlockedUsers,
+    } = useChat();
+
+    const fetchMessages = useCallback(async (interlocutor: User): Promise<MessageData[]> => {
+        let data = await axios.get(`http://localhost:3000/dm/${interlocutor.id}/messages`,
+            {
+                withCredentials: true,
+                validateStatus: () => true,
+            }
+        ).then((res) => { // Not friends for example
+            if (res.status === 401) {
+                setMissingPermissions(true);
+                return [];
+            } else if (res.status === 403) {
+                return [];
+            } else {
+                setMissingPermissions(false);
+            }
+            return res.data;
+        }).catch((err) => {
+            throw Error("UNEXPECTED ERROR: " + err);
+        })
+        data.forEach((message: Message) => {
+            message.timestamp = new Date(message.timestamp);
+        });
+        return data;
+    }, []);
+
+    useEffect(() => {
+        socket.emit('join-dm', channel.id);
+        socket.on('message', (payload: MessageData) => {
+            // parse the timestamp
+            payload.timestamp = new Date(payload.timestamp);
+            setMessages((messages) => [payload, ...messages]);
+        });
+        socket.on('messageDeleted', (payload: MessageData) => {
+            // find the message in the list and remove it
+            setMessages((messages) => {
+                const index = messages.findIndex((message) => message.message_id === payload.message_id);
+                if (index !== -1) {
+                    messages.splice(index, 1);
+                }
+                return [...messages];
+            });
+        });
+        socket.on('joinChannel', (payload: any) => {
+            fetchMessages(channel).then((messages) => {
+                setMessages(messages);
+            });
+        });
+        socket.on('staff', (staff: ChannelStaff) => {
+            setOwnerId(staff.owner_id);
+            setAdmins(new Set(staff.administrators));
+        });
+        socket.on("punishment", (punishment: PunishmentData) => {
+            // TODO: handle the punishment
+            switch (punishment.punishment_type) {
+                case "muted":
+                    setMutedChannels((mutedChannels) => {
+                        return new Set(mutedChannels).add(punishment.channel_id);
+                    });
+                    break;
+                case "banned":
+                    setBannedChannels((bannedChannels) => {
+                        return new Set(bannedChannels).add(punishment.channel_id);
+                    });
+                    break;
+                case "kicked":
+                    break;
+            }
+        });
+        fetchMessages(channel).then((messages) => {
+            setMessages(messages);
+        });
+        return () => {
+            socket.off('message');
+            socket.off('joinChannel');
+            socket.off('staff');
+            socket.off('punishment');
+        }
+    }, [socket, channel]);
+
+    const handleNewMessage = useCallback((message: string) => {
+        try {
+            axios.post(`http://localhost:3000/channel/${channel.id}/message`, { content: message }, { withCredentials: true })
+        } catch (err) {
+            throw Error("UNEXPECTED ERROR: " + err);
+        }
+    }, [channel]);
+
+    const memoizedMessages = useMemo(() => messages, [messages]);
+
+    // The user is banned
+    if (bannedChannels.has(channel.id)) {
+        return (
+            <div className="flex flex-col items-center justify-center h-full">
+                <h1 className="text-3xl font-bold">You are banned from this channel</h1>
+            </div>
+        )
+    }
+
+    // The user doesn't have access to this password protected channel, we need to ask for a password
+    if (missingPermissions && channel.password === "") {
+        return (
+            <ChannelPasswordPrompt channel={channel} />
+        );
+    } else if (missingPermissions && !channel.password) { // The user doesn't have access to this channel and it's not password protected
+        return ( // Throw a fake 404 message because it is a hidden channel
+            <div className="flex flex-col items-center justify-center h-full">
+                <h1 className="text-3xl font-bold">Unknown room</h1>
+            </div>
+        )
+    }
+
+    return (
+        <Container>
+            <Grid.Container gap={2} justify="center" css={{ height: "90vh" }}>
+                <Grid xs={12}>
+                    <Grid.Container>
+                        <Grid css={{ w: "stretch" }}>
+                            <Container direction="row" justify="space-between" alignItems="center" display="flex">
+                                <Text h3>{channel.name.replace(/^/, '# ')}</Text>
+                                <Button auto light onPress={() => setPowerModalOpen(true)}>
+                                    <IconShieldCog />
+                                </Button>
+                            </Container>
+                            <ul
+                                style={{
+                                    listStyle: "none",
+                                    padding: 0,
+                                    overflowY: "auto",
+                                    height: "80vh",
+                                    display: "flex",
+                                    flexDirection: "column-reverse",
+                                }}
+                            >
+                                {memoizedMessages.map((message: MessageData, index: number) => (
+                                    <li key={message.message_id} className="relative">
+                                        <ChatMessage
+                                            senderOwner={message.sender.id === ownerId}
+                                            senderAdmin={admins.has(message.sender.id)}
+                                            isOwner={user.id === ownerId}
+                                            isAdmin={admins.has(user.id)}
+                                            isAuthor={message.sender.id === user.id}
+                                            blocked={blockedUsers.has(message.sender.id)}
+                                            sender={message.sender}
+                                            channel={channel}
+                                            key={message.message_id}
+                                            data={message}
+                                            concatenate={
+                                                index != memoizedMessages.length - 1
+                                                && message.sender.id === memoizedMessages[index + 1].sender.id
+                                                && message.timestamp.getTime() - memoizedMessages[index + 1].timestamp.getTime() < 5 * 60 * 1000
+                                            }
+                                        />
+                                    </li>
+                                ))}
+                            </ul>
+                        </Grid>
+                        <Grid xs={12}>
+                            {(!missingPermissions && !bannedChannels.has(channel.id)) && (
+                                <Textarea
+                                    fullWidth
+                                    disabled={mutedChannels.has(channel.id)}
+                                    placeholder={
+                                        !mutedChannels.has(channel.id) ? `Send a message to #${channel.name}`
+                                        :
+                                        generateMutedMessage(455445455) // TODO: get duration from mutedChannels / bannedChannels
+                                    }
+                                    aria-label={
+                                        !mutedChannels.has(channel.id) ? `Send a message to the channel : ${channel.name}`
+                                        :
+                                        generateMutedMessage(455445455) // TODO: get duration from mutedChannels / bannedChannels
+                                    }
+                                    minLength={1}
+                                    maxLength={2000}
+                                    onKeyPress={(e: any) => {
+                                        if (e.key === "Enter" && !e.shiftKey) {
+                                            e.preventDefault();
+                                            let message: string = e.target.value;
+                                            message = message.trim();
+                                            if (message.length > 0) {
+                                                handleNewMessage(message);
+                                                e.target.value = "";
+                                            }
+                                        }
+                                    }}
+                                />
+                            )
+                            }
+                        </Grid>
+                    </Grid.Container>
+                </Grid>
+            </Grid.Container>
+        </Container>
+    );
+};
+
+export default React.memo(DMChatBox);
--- app/front/src/pages/chat/dm/[id].tsx
+++ app/front/src/pages/chat/dm/[id].tsx
@@ -1,7 +1,33 @@
-const DMChat: React.FC = () => {
+import { useRouter } from "next/router";
+import ChatLayout from "../layout";
+import { Channel } from "@/interfaces/chat.interfaces";
+import { useChat } from "@/contexts/chat.context";
+import ChatBox from "@/components/chat/ChatBox";
+
+const ChannelChat: React.FC = () => {
+    const router = useRouter();
+    const { id } = router.query;
+    const { channels } = useChat();
+
+    // Convert string to number
+    const channelId: number = typeof id === "string" ? parseInt(id) : -1;
+    const channel: Channel | undefined = channels.find((channel) => channel.id === channelId);
+
+    if (!channel) {
+        return (
+            <ChatLayout>
+                <div className="flex flex-col items-center justify-center h-full">
+                    <h1 className="text-3xl font-bold">404</h1>
+                    <p className="text-xl">Could not find the channel you are looking for.</p>
+                </div>
+            </ChatLayout>
+        );
+    }
     return (
-        <h1>Hello world (dm)</h1>
+        <ChatLayout>
+            <ChatBox channel={channel} />
+        </ChatLayout>
     );
 }
 
-export default DMChat;
+export default ChannelChat;
