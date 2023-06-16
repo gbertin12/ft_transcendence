e70cb5323666b5afce0a0b1b1e9f984ceb463ba1
Jonas ALLERHAND <jallerha@student.42.fr>
2023-06-16 16:13:55 +0200

    rm: useless import

--- app/front/src/components/chat/FriendList.tsx
+++ app/front/src/components/chat/FriendList.tsx
@@ -3,7 +3,6 @@ import { Text, Grid } from "@nextui-org/react";
 import ChatFriendBrowser from "./ChatFriendBrowser";
 import FriendRequests from "./FriendRequests";
 import { Friend } from "@/interfaces/chat.interfaces";
-import { useSocket } from "@/contexts/socket.context";
 
 
 const FriendList: React.FC<any> = () => {
